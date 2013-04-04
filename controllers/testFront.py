13066ec4bb903bef5cc601c8b0cb40ddb8412891
linkup89 <piyomo@gmail.com>
2013-04-04 13:02:32 -0400

    Working basic sql form for persona

--- controllers/testFront.py
+++ controllers/testFront.py
@@ -2,5 +2,10 @@
 __author__ = 'Evolutiva'
 
 def index():
-    a_form = SQLForm(db.persona)
+    a_form = SQLFORM(db.persona)
+    if a_form.process().accepted:
+       response.flash = 'form accepted'
+    elif a_form.errors:
+       response.flash = 'form has errors'
+
     return dict(form= a_form)
