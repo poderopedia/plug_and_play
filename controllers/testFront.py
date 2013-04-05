16118645c99e6b3766f77ab66cc3ee7ddcb68ca9
linkup89 <piyomo@gmail.com>
2013-04-04 21:55:13 -0400

    Basic working form and listing

--- controllers/testFront.py
+++ controllers/testFront.py
@@ -1,11 +1,52 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 __author__ = 'Evolutiva'
 
-def index():
-    a_form = SQLFORM(db.persona)
-    if a_form.process().accepted:
-       response.flash = 'form accepted'
-    elif a_form.errors:
-       response.flash = 'form has errors'
 
-    return dict(form= a_form)
+def index():
+
+    my_dict = dict()
+
+
+    my_dict['a_error']=''
+
+    label_dict = dict(ICN='Rut', firstLastName='Apellido Paterno',
+                      otherLastName='Apellido Materno')
+    fields_dict = [
+        'ICN',
+        'firstName',
+        'firstLastName',
+        'otherLastName',
+        'alias',
+        'birth',
+        'shortBio',
+        'countryofResidence',
+        'depiction',
+    ]
+
+    # hidden_dict = dict(state_publication='draft',date_publication=request.now,
+        # state_colaboration=False)
+
+    a_form = SQLFORM(db.persona, labels=label_dict, fields=fields_dict)
+
+    # a_form.vars['state_publication']='draft'
+    # a_form.vars['date_publication']=request.now
+    # a_form.vars['state_colaboration']=False
+
+    if a_form.process().accepted:
+        # response.flash = 'form accepted'
+        redirect(URL('accepted'))
+    elif a_form.errors:
+        my_dict['a_error'] = "Ocurrio un error"
+        response.flash = 'form has errors'
+
+    my_dict['form']=a_form
+    return my_dict
+
+def accepted():
+    return dict()
+
+def display():
+    grid = SQLFORM.grid(db.persona,editable=True,user_signature=False)
+    return locals()
