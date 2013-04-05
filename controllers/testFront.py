59b8ff7434de2dcc0ea5c561d985bea4582201eb
demianschkolnik <demianschkolnik@gmail.com>
2013-04-05 12:33:57 -0400

    vistas dumm

--- controllers/testFront.py
+++ controllers/testFront.py
@@ -2,8 +2,22 @@
 __author__ = 'Evolutiva'
 
 def index():
+    #implementa plantilla main
     return locals()
 
 
 def publicaciones_general():
+    #grilla publicaciones general
+    return locals()
+    
+def paginas_general():
+    #grilla páginas general
+    return locals()
+    
+def publicaciones_empresas():
+    #grilla de publiaciones general
+    return locals()
+    
+def usuarios_general():
+    #lista de usuarios
     return locals()
