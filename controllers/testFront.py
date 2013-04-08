7739fdacc7fb5dcd81e81ca0a482dee9b3704caa
sorej <heartlessjeros@gmail.com>
2013-04-08 10:05:40 -0400

    agregada grilla para quick profile en testFront.py

--- controllers/testFront.py
+++ controllers/testFront.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 __author__ = 'Evolutiva'
 
 def index():
@@ -9,3 +9,7 @@ def index():
        response.flash = 'form has errors'
 
     return dict(form= a_form)
+
+def grid():
+    a_grid = SQLFORM.grid(db.auth_user,user_signature=False)
+    return dict(grid= a_grid)
