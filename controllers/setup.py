6edd22fbf1870208c8f95f986618ea0014ce515f
demianschkolnik <demianschkolnik@gmail.com>
2013-04-17 09:48:32 -0400

    Script de setup

---
+++ controllers/setup.py
@@ -0,0 +1,8 @@
+# coding: utf8
+# intente algo como
+def index(): 
+    from setup import agregar_rol_autor, agregar_rol_colaborador, agregar_rol_editor
+    agregar_rol_autor()
+    agregar_rol_colaborador()
+    agregar_rol_editor()
+    return dict()
