3244855b5209b5c9cd9042420e7a278340728efe
sorej <heartlessjeros@gmail.com>
2013-05-02 11:52:10 -0400

    publication scheduling almost working

--- controllers/editor.py
+++ controllers/editor.py
@@ -114,27 +114,30 @@ def reject_persona():
 
 @auth.requires(auth.has_membership(group_id = 'superadmin') or auth.has_membership(group_id = 'admin') or auth.has_membership(group_id = 'editor'))
 def schedule_persona():
-
+ # Formulario de ingreso de sugerencia para personas
+    my_dict = dict()
     my_dict['a_error'] = ''
-
+    a_form = ''
+    db.persona.date_publication.writable=True; 
     if 'id' in request.vars:
         a_id = request.vars['id']
         db.persona.state_collaboration.default = 'acccepted'
         db.persona.state_publication.default = 'programmed'
+        label_dict = dict(date_publication=T('Fecha de Publicación'))
         fields_dict = ['date_publication']
 
+        a_form = SQLFORM(db.persona,a_id, labels=label_dict, fields=fields_dict,submit_button=T('Programar Publicación'))
 
-        a_form = SQLFORM(db.persona,a_id,fields_dict);
-        
         if a_form.process().accepted:
-            response.flash = 'Su publicación se hará pública en la fecha elegida'
+            response.flash = T('Su publicación se hará pública en la fecha elegida')
         elif a_form.errors:
-            my_dict['a_error'] = 'Oops! ocurrió un error'
-            response.flash = 'Hay errores en el formulario'
+            my_dict['a_error'] = T('Hay un error en el formulario')
+            response.flash = 'Formulario con errores'
 
-        my_dict['form'] = a_form
-
+    my_dict['form'] = a_form
     return my_dict
+
+
 
 @auth.requires(auth.has_membership(group_id = 'superadmin') or auth.has_membership(group_id = 'admin') or auth.has_membership(group_id = 'editor'))
 def display_organizacion():
@@ -240,7 +243,7 @@ def reject_organizacion():
 
 @auth.requires(auth.has_membership(group_id = 'superadmin') or auth.has_membership(group_id = 'admin') or auth.has_membership(group_id = 'editor'))
 def schedule_organizacion():
-
+    my_dict = dict()
     my_dict['a_error'] = ''
 
     if 'id' in request.vars:
