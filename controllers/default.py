584ca0fa3f60e5a9e3abfa5836f7fd3ecba49f66
linkup89 <piyomo@gmail.com>
2013-05-02 17:45:52 -0400

    New users are collaborator on default, create auth_groups automatically

--- controllers/default.py
+++ controllers/default.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 ### required - do no delete
-def user(): return dict(form=auth())
+def user(): 
+
+    groupadd('super_admin')
+    groupadd('admin')
+    groupadd('editor')
+    groupadd('author')
+    groupadd('collaborator')
+    if request.args(0) == 'register':
+        auth.settings.create_user_groups = False
+        auth.settings.register_onaccept=[lambda form:
+        auth.add_membership(auth.id_group('collaborator'),form.vars.id)]
+    return dict(form=auth())
+
 def download(): return response.download(request,db)
 def call(): return service()
 ### end requires
@@ -16,3 +28,8 @@ def error():
 
 def inicio():
     return dict()
+
+def groupadd(check_group):
+    if not db(db.auth_group.role==check_group).count():
+     db.auth_group.insert(role=check_group)
+
