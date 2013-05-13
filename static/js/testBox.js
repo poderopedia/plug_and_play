a8dfc759e89f4a0e747069849aba015e01bc75a6
demianschkolnik <demianschkolnik@gmail.com>
2013-05-13 11:46:13 -0400

    commit3

---
+++ static/js/testBox.js
@@ -0,0 +1,23 @@
+     $('#inputbox').triggeredAutocomplete({
+   hidden: '#hidden_inputbox',
+    source: [
+ "Asp",
+ "BASIC",
+ "COBOL",
+ "ColdFusion",
+ "Erlang",
+ "Fortran",
+ "Groovy",
+ "Java",
+ "JavaScript",
+ "Lisp",
+ "Perl",
+ "PHP",
+ "Python",
+ "Ruby",
+ "Scala",
+ "Scheme"
+ ],
+   trigger: "@" 
+});
+
